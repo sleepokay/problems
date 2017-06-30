e285713895e8002535c8d207c3be8f86372b29ef
sleepokay <sleepokay@gmail.com>
2017-06-30 15:33:11 -0400

    add solutions to merge and compare lists

--- LinkedList.py
+++ LinkedList.py
@@ -19,3 +19,50 @@ def ReverseHelp(node, prev):
     temp = node.next
     node.next = prev
     return ReverseHelp(temp, node)
+
+
+# check two linked lists for equality
+def CompareLists(nodeA, nodeB):
+    while nodeA or nodeB:
+        if nodeA and nodeB and nodeA.data == nodeB.data:
+            nodeA = nodeA.next
+            nodeB = nodeB.next
+        else: 
+            return 0    
+    return 1
+
+
+# merge two sorted linked lists in ascending order
+def MergeLists(nodeA, nodeB): 
+    new_head = None
+    if nodeA and nodeB:
+        if nodeA.data < nodeB.data:
+            new_head = nodeA
+            nodeA = nodeA.next
+        else:
+            new_head = nodeB
+            nodeB = nodeB.next
+    elif not nodeA:
+        return nodeB
+    elif not nodeB:
+        return nodeA
+    
+    current = new_head
+    
+    while nodeA or nodeB:       
+        if not nodeA:
+            current.next = nodeB
+            return new_head
+        elif not nodeB:
+            current.next = nodeA
+            return new_head
+        elif nodeA.data < nodeB.data:
+            current.next = nodeA
+            nodeA = nodeA.next
+            current = current.next
+        elif nodeA.data >= nodeB.data:
+            current.next = nodeB
+            nodeB = nodeB.next
+            current = current.next
+            
+    return new_head
