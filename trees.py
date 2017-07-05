0010bc8796cced78e12267ed5391f1c2cb685e76
sleepokay <sleepokay@gmail.com>
2017-07-05 15:53:32 -0400

    solved height for binary trees

--- trees.py
+++ trees.py
@@ -16,3 +16,23 @@ def postOrder(root):
     postOrder(root.right)
     
     sys.stdout.write(str(root.data) + " ")
+
+
+def inOrder(root):
+    if not root:
+        return
+    
+    inOrder(root.left)
+    sys.stdout.write(str(root.data) + " ")
+    inOrder(root.right)
+
+# find height of binary tree
+def height(node):
+    if not node:
+        return -1
+    
+    left_height = 1 + height(node.left)
+    right_height = 1 + height(node.right)
+    
+    return max(left_height, right_height)
+    
